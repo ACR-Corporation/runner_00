0761446a80c8836a1fca4f004bb5cee0745d4a7f
Pierre-Chesne <pierrc@hotmail.com>
2023-12-18 11:52:46 +0100

    vm runner

--- az_cli/gh.sh
+++ az_cli/gh.sh
@@ -15,6 +15,6 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
 
-./config.sh --unattended --url https://github.com/${REPO} --token ${REG_TOKEN} --labels test-runner
+./config.sh --unattended --url https://github.com/${REPO} --token ${REG_TOKEN} --labels test-self-runner
 ./run.sh
 
