04c6c48fc61b436fb7e11478364046cae593810b
Pierre-Chesne <pierrc@hotmail.com>
2023-12-18 13:57:15 +0100

    vm

--- az_cli/gh.sh
+++ az_cli/gh.sh
@@ -2,7 +2,7 @@
 
 # Variables
 REPO=ACR-Corporation/runner_00
-ACCESS_TOKEN=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
+ACCESS_TOKEN=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
 
 # Installation de jq
 sudo apt-get install jq -y
@@ -11,9 +11,11 @@ sudo apt-get install jq -y
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 
-mkdir actions-runner && cd actions-runner
+cd /home/pierrc && mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.311.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.311.0/actions-runner-linux-x64-2.311.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.311.0.tar.gz
+
+RUN chown -R pierrc ~pierrc && /home/pierrc/actions-runner/bin/installdependencies.sh
 
 ./config.sh --unattended --url https://github.com/${REPO} --token ${REG_TOKEN} --labels test-self-runner
 ./run.sh
